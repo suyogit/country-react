01b2d9b2270a8094a775ae38a22e0454ccc59f51
Suyog Acharya <asuyog042@gmail.com>
2024-10-07 11:35:04 +0545

    nav bar built

---
+++ components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <div className="nav-child">
+            <a href="index.html">
+                <h2 className="title">Where in the world?</h2>
+            </a>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Header
